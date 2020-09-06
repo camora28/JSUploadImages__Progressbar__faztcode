b27ef9c547844819276a31dec76819aba7519071
Camora28@ <camora88@gmail.com>
2020-09-06 06:25:46 -0500

    falla metodo post con axios

---
+++ app.js
@@ -0,0 +1,19 @@
+const imagePreview = document.getElementById('img-preview')
+const imageUploader = document.getElementById('img-uploader')
+
+const CLOUDINARY_URL = 'https://api.cloudinary.com/v1_1/camora28/image/upload';
+const CLOUDINARY_UPLOAD_PRESET = 'sloch0n0';
+
+imageUploader.addEventListener('change', async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_present', CLOUDINARY_UPLOAD_PRESET);
+    
+    const res = await axios.post(CLOUDINARY_URL, formData, {
+        headers: {
+            'content-type': 'multipart/form-data'
+        }
+    });
+    console.log(res);
+})
